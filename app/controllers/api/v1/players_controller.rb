2e43d19bb188947b5dc30128c046a47a8c154f77
kbberker <kbbuk90@gmail.com>
2019-01-30 16:40:12 +0000

    Added index, show and create methods and player_params private method

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::PlayersController < ApplicationController
+
+  def index
+    @players = Player.all
+    render json: @players 
+  end
+
+  def show
+    @player = Player.find_by(id: params[:id])
+    render json: @player
+  end
+
+  def create
+    @player = Player.new(play)
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:name, :is_dasher)
+  end
+
 end
