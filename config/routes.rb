0740b0ff74303f200717ae5ddde8e00edd12954b
kbberker <kbbuk90@gmail.com>
2019-01-30 17:13:24 +0000

    Add games_controller and resources for games, rounds, answers in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       resources :players
+      resources :games
+      resources :rounds
+      resources :answers
     end
   end
 end
